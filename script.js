91fc35254a009c5b852ebc06d503000aba080a1f
NishanthD32 <nishanth323232@gmail.com>
2023-06-20 09:49:38 +0530

    callback

---
+++ script.js
@@ -0,0 +1,52 @@
+let image = document.createElement('img');
+image.setAttribute('id','image');
+image.src="https://static.vecteezy.com/system/resources/thumbnails/016/729/927/small/india-flag-background-with-brush-style-and-halftone-effect-indian-tricolor-national-flag-illustration-with-grunge-concept-vector.jpg"
+document.body.appendChild(image);
+
+
+setTimeout(()=>{
+    let h1 = document.createElement('h1')
+    h1.setAttribute('id','h1')
+    h1.innerHTML="10"
+    document.body.appendChild(h1)
+
+    setTimeout(()=>{
+        h1.innerHTML="9"
+
+        setTimeout(()=>{
+            h1.innerHTML="8"
+
+            setTimeout(()=>{
+                h1.innerHTML="7"
+
+                setTimeout(()=>{
+                    h1.innerHTML="6"
+
+                    setTimeout(()=>{
+                        h1.innerHTML="5"
+
+                        setTimeout(()=>{
+                            h1.innerHTML="4"
+
+                            setTimeout(()=>{
+                                h1.innerHTML="3"
+
+                                setTimeout(()=>{
+                                    h1.innerHTML="2"
+
+                                    setTimeout(()=>{
+                                        h1.innerHTML="1"
+
+                                        setTimeout(()=>{
+                                            h1.innerHTML="Happy Independance Day"
+                                        },1000)
+                                    },1000)
+                                },1000)
+                            },1000)
+                        },1000)
+                    },1000)
+                },1000)
+            },1000)
+        },1000)
+    },1000)
+},1000)
